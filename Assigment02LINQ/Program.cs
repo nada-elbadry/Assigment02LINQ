313652117b44db2138a9ef4b97e87eb6fd4c7949
Your Name <Nada Elbadry nadaelbadry1012@gmail.com>
2025-09-10 22:19:12 +0300

    Add project files.

---
+++ Assigment02LINQ/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assigment02LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
